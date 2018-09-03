887f5784b5c625b56439d3d03c5f4c817fa93c9d
张晓旭 <itmuzi@aliyun.com>
2018-09-03 16:22:11 +0800

    生成静态首页

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -24,23 +24,14 @@ class BlogController
         //  实例化对象
         $blogs = new Blog;
         $blogs = $blogs->content_2_html();
-
-        //  生成静态页
-        foreach($blogs as $v)
-        {
-            //  加载视图
-            view('blogs.content',[
-                'blogs'=>$v
-            ]);
-
-            //  取出缓冲区的内容
-            $str = ob_get_contents();
-            //  生成静态页
-            file_put_contents(ROOT.'/public/contents/'.$v['id'].'.html',$str);
-            //  清空缓冲区
-            ob_clean();
-        }
-
        
+    }
+
+    //  生成首页
+    public function content_to_index()
+    {
+        //  实例化对象
+        $blogs = new Blog;
+        $blogs = $blogs->content_2_index();
     }
 }
